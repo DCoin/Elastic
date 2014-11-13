b368523008aa5639e45797f68bc7e9bf41da66e5
kasraman1000 <minutegreywing@gmail.com>
2014-11-13 21:24:08 +0100

    Added Input and Basic Controls
    ControllerManager added, query it for input! PlayerController got basic
    implementation going

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,57 @@ using InControl;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 10f;
+	public Collider2D jumpCollider;
+
+	public float moveSpeed = 10f;
+	public float jumpSpeed = 3f;
+
+
 	public int controller = 0;
+	public bool leftSide = true;
+
+
+	private bool onGround = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+		//if (ControllerManager.GetJumpInput(controller, leftSide)) print ("Left stick jump!");
+		//if (ControllerManager.GetJumpInput(controller, !leftSide)) print ("Right stick jump!");
+
+		// Horizontal movement
+		rigidbody2D.AddForce(Vector2.right * ControllerManager.GetHorizontalInput(controller, leftSide) * moveSpeed);
+
+		// Jumping
+		if (onGround){
+			if (ControllerManager.GetJumpInputBool(controller, leftSide)) {
+				rigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+			}
+		}
+
+		/*
 		var inputDevice = (InputManager.Devices.Count > controller) ? InputManager.Devices[controller] : null;
 		if (inputDevice == null) {
 			//TODO Do something appropriate
 		} else {
 			transform.Translate(Vector2.right * speed * Time.deltaTime * inputDevice.RightStickX.Value); // THIS IS SHIT AND MUST GO AWAY!!!
 		}
+		*/
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		// TODO check if it's platform, not just any object
+		Debug.Log("On ground");
+		onGround = true;
+	}
+
+	void OnTriggerExit2D(Collider2D col) {
+		// TODO check if it's platform, not just any object
+		Debug.Log("Leaving ground");
+		onGround = false;
 	}
 }
