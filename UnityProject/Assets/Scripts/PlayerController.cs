125b3e09d5b08e9eb0d1043699019674f22b5691
DCoin <pokedcoin@gmail.com>
2014-11-13 13:55:16 +0100

    Added support for multiple controllers

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using InControl;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 10f;
+	public int controller = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,11 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		InputDevice activeDevice = InputManager.ActiveDevice;
-		transform.Translate(Vector2.right*speed*Time.deltaTime*activeDevice.RightStickX.Value); // THIS IS SHIT AND MUST GO AWAY!!!
+		var inputDevice = (InputManager.Devices.Count > controller) ? InputManager.Devices[controller] : null;
+		if (inputDevice == null) {
+			//TODO Do something appropriate
+		} else {
+			transform.Translate(Vector2.right * speed * Time.deltaTime * inputDevice.RightStickX.Value); // THIS IS SHIT AND MUST GO AWAY!!!
+		}
 	}
 }
