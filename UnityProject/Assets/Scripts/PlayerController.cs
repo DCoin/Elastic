e1f3ef0385e38ae9ab644c51bd016bd425046112
DCoin <pokedcoin@gmail.com>
2014-11-13 12:39:48 +0100

    Added PlayerController and InControl support
    Added very simple player controller and updated the player prefab and
    scene1 to use it and InControl (Each scene must contain an InControl
    manager object).

---
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		InputDevice activeDevice = InputManager.ActiveDevice;
+		transform.Translate(Vector2.right*speed*Time.deltaTime*activeDevice.RightStickX.Value); // THIS IS SHIT AND MUST GO AWAY!!!
+	}
+}
