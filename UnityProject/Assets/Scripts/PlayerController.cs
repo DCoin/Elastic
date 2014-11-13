e699944714f51f8ebb9b4f8d9f80a49e1fff7e66
kasraman1000 <minutegreywing@gmail.com>
2014-11-13 21:25:27 +0100

    Cleaned up PlayerController

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,11 @@ using System.Collections;
 using InControl;
 
 public class PlayerController : MonoBehaviour {
-
-	public Collider2D jumpCollider;
-
 	public float moveSpeed = 10f;
 	public float jumpSpeed = 3f;
 
-
 	public int controller = 0;
 	public bool leftSide = true;
-
 
 	private bool onGround = false;
 
@@ -23,9 +18,6 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		//if (ControllerManager.GetJumpInput(controller, leftSide)) print ("Left stick jump!");
-		//if (ControllerManager.GetJumpInput(controller, !leftSide)) print ("Right stick jump!");
-
 		// Horizontal movement
 		rigidbody2D.AddForce(Vector2.right * ControllerManager.GetHorizontalInput(controller, leftSide) * moveSpeed);
 
@@ -35,26 +27,15 @@ public class PlayerController : MonoBehaviour {
 				rigidbody2D.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
 			}
 		}
-
-		/*
-		var inputDevice = (InputManager.Devices.Count > controller) ? InputManager.Devices[controller] : null;
-		if (inputDevice == null) {
-			//TODO Do something appropriate
-		} else {
-			transform.Translate(Vector2.right * speed * Time.deltaTime * inputDevice.RightStickX.Value); // THIS IS SHIT AND MUST GO AWAY!!!
-		}
-		*/
 	}
 
 	void OnTriggerEnter2D(Collider2D col) {
 		// TODO check if it's platform, not just any object
-		Debug.Log("On ground");
 		onGround = true;
 	}
 
 	void OnTriggerExit2D(Collider2D col) {
 		// TODO check if it's platform, not just any object
-		Debug.Log("Leaving ground");
 		onGround = false;
 	}
 }
