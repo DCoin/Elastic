54ff439e19a4016bffb1c77901cd290ee475d830
DCoin <pokedcoin@gmail.com>
2014-11-17 15:40:15 +0100

    Updated controls
    Heavy drag is now only horizontal and movement is disabled while heavy

--- UnityProject/Assets/Scripts/PlayerController.cs
+++ UnityProject/Assets/Scripts/PlayerController.cs
@@ -10,14 +10,14 @@ public class PlayerController : MonoBehaviour {
 	public float jumpDelay = 0.5f;
 
 	public float heavyMultiplier = 5;
-	public float heavyDrag = 1;
+	public float heavyDrag = 0.02f;
 
 	public int controller = 0;
 	public bool leftSide = true;
 
 	private bool onGround = false;
 	private float baseMass;
-	private float baseDrag;
+
 	// We store the state of the heavy key here because inputControl.wasPressed() is not reliable
 	private bool isHeavy = false;
 	private float lastJump = 0f;
@@ -28,16 +28,18 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		baseMass = rigidbody2D.mass;
-		baseDrag = rigidbody2D.drag;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		// Horizontal movement
-		print("Velo: " + rigidbody2D.velocity.x); // TODO remove once moved feels good
-		// TODO add if that checks if there is any input at all.
-		var dir = ControllerManager.GetRightInputBool(controller, leftSide) ? 1 : -1; // Will always show left on no input but that doesnt matter as its 0.
-		rigidbody2D.AddForce(Vector2.right * ControllerManager.GetHorizontalInput(controller, leftSide) * GetAdjustedAcceleration(rigidbody2D.velocity.x * dir), impulse?ForceMode2D.Impulse:ForceMode2D.Force);
+		// TODO Is the Mathf check that checks if there is any input woth it?
+		// TODO Should be based on the speed of the platform you are on.
+		// TODO Add different air control?
+		if (!isHeavy && !Mathf.Approximately(ControllerManager.GetHorizontalInput(controller, leftSide), 0f)) {
+			var dir = ControllerManager.GetRightInputBool(controller, leftSide) ? 1 : -1; // Will always show left on no input but that doesnt matter as its 0.
+			rigidbody2D.AddForce(Vector2.right * ControllerManager.GetHorizontalInput(controller, leftSide) * GetAdjustedAcceleration(rigidbody2D.velocity.x * dir), impulse?ForceMode2D.Impulse:ForceMode2D.Force);
+		}
 
 		// Jumping
 		// TODO varied jump
@@ -54,15 +56,18 @@ public class PlayerController : MonoBehaviour {
 		if (ControllerManager.GetHeavyInputBool(controller, leftSide)) {
 			if (!isHeavy) {
 				rigidbody2D.mass = baseMass * heavyMultiplier;
-				rigidbody2D.drag = baseDrag + heavyDrag; //TODO does this feel good?
 				isHeavy = true;
 			}
 		} else {
 			if (isHeavy) {
 				rigidbody2D.mass = baseMass;
-				rigidbody2D.drag = baseDrag;
 				isHeavy = false;
 			}
+		}
+		if (isHeavy) {
+			var vel = rigidbody2D.velocity;
+			vel.x *= 1 - heavyDrag;
+			rigidbody2D.velocity = vel;
 		}
 	}
 
