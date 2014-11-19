feed97ef9d26f58ca23da3e601c2a46c9b1ebc7f
Elgeorgio <sjorsmartens_2@hotmail.com>
2014-11-19 17:33:58 +0100

    Crappy Menu screen
    Added a crappy menu screen.

---
+++ UnityProject/Assets/Scripts/TextControl.js
@@ -0,0 +1,32 @@
+﻿#pragma strict
+
+function Start () {}
+
+var IsTutorialButton = false;
+var IsLevelButton = false;
+var IsQuitButton = false;
+	
+function OnMouseEnter()
+{
+	renderer.material.color = Color.green;
+}
+
+function OnMouseExit()
+{
+renderer.material.color = Color.white;
+} 
+
+function OnMouseUp()
+{
+	if (IsTutorialButton) {
+			Application.LoadLevel(1);
+				} 
+	else if (IsLevelButton) {
+			Application.LoadLevel(2);
+				}
+	else if (IsQuitButton){
+			Application.Quit ();
+				}
+		}
+
+
