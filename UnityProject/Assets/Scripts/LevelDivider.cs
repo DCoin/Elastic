c77a3cf505a1b59663b3e8146629ba9551a540ad
aholdt <alexanderholdt@gmail.com>
2014-11-20 14:06:05 +0100

    LevelDivider prefab to put in between levels where you require the object pickup

---
+++ UnityProject/Assets/Scripts/LevelDivider.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelDivider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.transform.root.Find ("Rope2")) {
+				transform.collider2D.isTrigger = true;
+				}
+	}
+
+	void OnTriggerExit2D(Collider2D col) 
+	{
+		StartCoroutine (WaitAndSetTrigger(col.transform.root));
+	}
+
+	IEnumerator WaitAndSetTrigger(Transform squad)
+	{  
+		yield return new WaitForSeconds(3);
+		Mesh mesh = GetComponent<MeshFilter> ().mesh;
+		float width = mesh.bounds.size.x;
+		bool removetrigger = true;
+		foreach (Collider2D col in squad.GetComponentsInChildren<Collider2D>()) {
+			if (col.transform.position.x > transform.position.x-width/2 && col.transform.position.x < transform.position.x+width/2) {
+				removetrigger = false;
+				break;
+			}
+		}
+		if (removetrigger) {
+						transform.collider2D.isTrigger = false;
+		} else {  WaitAndSetTrigger(squad); }
+	}
+}
