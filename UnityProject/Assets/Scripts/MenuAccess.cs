3e8a31cde47139704904558251eb3c52bf59a343
DCoin <pokedcoin@gmail.com>
2014-11-21 11:59:55 +0100

    Menu are now its on gameObject

---
+++ UnityProject/Assets/Scripts/MenuAccess.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuAccess : MonoBehaviour {
+
+	void Update () {
+		// Go to menu on escape
+		if (Input.GetKey(KeyCode.Escape)) Application.LoadLevel(0);
+	}
+}
