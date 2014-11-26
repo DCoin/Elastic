8990176a8d6e9a05c1cadfda4c5badf231332076
kasraman1000 <minutegreywing@gmail.com>
2014-11-26 15:10:17 +0100

    Added HATS!!!

--- UnityProject/Assets/Scripts/EyeAnimator.cs
+++ UnityProject/Assets/Scripts/EyeAnimator.cs
@@ -11,15 +11,25 @@ public class EyeAnimator : MonoBehaviour {
 
 	public Sprite eyeballSprite;
 	public Sprite irisSprite;
+	public Color irisColor = Color.blue;
+	public HatManager.HatNames hat;
+
+	private Sprite hatSprite;
 
 	private int controllerID;
 	private bool leftSide;
 
 	private GameObject go_eyeball;
 	private GameObject go_iris;
+	private GameObject go_hat;
 
 	private SpriteRenderer sr_eyeball;
 	private SpriteRenderer sr_iris;
+	private SpriteRenderer sr_hat;
+
+	// how should velocity be scaled before moving hats accordingly?
+	private const float HAT_MOVE_RATIO_X = 0.05f;
+	private const float HAT_MOVE_RATIO_Y = 0.02f;
 
 	// Use this for initialization
 	void Start () {
@@ -31,6 +41,7 @@ public class EyeAnimator : MonoBehaviour {
 
 		var playerController = gameObject.GetComponent<PlayerController>();
 
+		// TODO use the built in unity things to check if there's a playercontroller
 		if (!playerController) {
 			Debug.LogError("No PlayerController on this GameObject.");
 			enabled = false;
@@ -44,51 +55,125 @@ public class EyeAnimator : MonoBehaviour {
 		// Create objects for sprites to be attached to
 		go_eyeball  = new GameObject();
 		go_iris 	= new GameObject();
+		go_hat		= new GameObject();
 
 		// Names
 		go_eyeball.name = "EyeballSprite";
 		go_iris.name = "IrisSprite";
+		go_hat.name = "HatSprite";
 
 		// Child them to this gameobject
 		go_eyeball.transform.parent = transform;
 		go_iris.transform.parent = transform;
+		go_hat.transform.parent = transform;
 
 		// Zero them according to parents
 		go_eyeball.transform.localPosition = Vector2.zero;
 		go_iris.transform.localPosition = Vector2.zero;
+		go_hat.transform.localPosition = Vector2.zero;
 
 		// Spriterenderers
 		sr_eyeball = go_eyeball.AddComponent<SpriteRenderer>();
 		sr_iris = go_iris.AddComponent<SpriteRenderer>();
+		sr_hat = go_hat.AddComponent<SpriteRenderer>();
 
-		// Assign sprites
+		// Get sprite for hat from Hat manager and assign to spriterenderer
+		ChangeHat(hat);
+
+		// Assign remaining sprites
 		sr_eyeball.sprite = eyeballSprite;
 		sr_iris.sprite = irisSprite;
 
-		// Sort to proper layers
+		// Set color of iris
+		SetIrisColor(irisColor);
+
+		// Sort in proper order
 		sr_eyeball.sortingOrder = 1;
 		sr_iris.sortingOrder = 2;
+		sr_hat.sortingOrder = 3;
 	}
 	
 	// We move the iris based on analog movement
 	void LateUpdate () {
+		// Split into several functions to declutter
+		MoveIris();
+		MoveHat();
+	}
+
+	// Move iris based on controller input
+	private void MoveIris() {
 		// TODO add an offset value, to prevent going to the edge
-
+		
 		// TODO squeeze Iris based on direction looking
-
+		
 		// Find value for max translation distance, mx and my
 		var eBounds = sr_eyeball.bounds;
 		var iBounds = sr_iris.bounds;
-
+		
 		float mx = eBounds.extents.x - iBounds.extents.x;
 		float my = eBounds.extents.y - iBounds.extents.y;
-
+		
 		// Get controlstick axis values
 		float x = ControllerManager.GetHorizontalInput(controllerID, leftSide);
 		float y = ControllerManager.GetVerticalInput(controllerID, leftSide);
-
+		
 		// Translate eye according to values
 		go_iris.transform.localPosition = new Vector2((x * mx), (y * my));
+	}
+
+	// move hat based on current velocity
+	private void MoveHat() {
+		// Find radius for hat origin
+		float radius = sr_eyeball.bounds.extents.y;
+
+		// Get velocity
+		Vector2 velocity = rigidbody2D.velocity;
+
+		// TODO simple naive implementation
+
+		// Use velocity to find distance away from eyeball
+		float xDist = -velocity.x * HAT_MOVE_RATIO_X;
+		float yDist = -velocity.y * HAT_MOVE_RATIO_Y;
+
+		// Find new point to put now, related to circumference
+		Vector2 newPos = Vector2.ClampMagnitude(
+			new Vector2(xDist, radius),
+			radius+yDist);
+
+		// Translate hat according to values
+		go_hat.transform.localPosition = newPos;
+
+		// Find rotation
+		float newAngle = Vector2.Angle(Vector2.up, newPos);
+
+		// flip rotation depending on direction
+		newAngle *= (velocity.x > 0) ? 1 : -1;
+
+		// Rotate hat according to values
+		go_hat.transform.rotation = Quaternion.identity;
+		go_hat.transform.Rotate(Vector3.forward, newAngle);
+
+		// TODO squeeze hat based on y movement
+	}
+
+	// TODO the following two methods set two seperate values, seems prone to error
+
+	/// <summary>
+	/// Changes the hat worn by this eye.
+	/// </summary>
+	/// <param name="hatName">Hat name.</param>
+	public void ChangeHat(HatManager.HatNames hatName) {
+		this.hatSprite = HatManager.Instance.GetHatSprite(hatName);
+		sr_hat.sprite = hatSprite;
+	}
+
+	/// <summary>
+	/// Sets the color of the iris.
+	/// </summary>
+	/// <param name="color">Color for the iris.</param>
+	public void SetIrisColor(Color color) {
+		this.irisColor = color;
+		sr_iris.color = color;
 	}
 
 	// Drawing in the scene view, for convenience
---
+++ UnityProject/Assets/Scripts/HatManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class HatManager : MonoBehaviour {
+
+	// One value for every hat we have
+	public enum HatNames
+	{
+		ArmyHat,
+		CowboyHat
+	}
+
+	[System.Serializable] // This makes it show up in the inspector, somehow
+	public class Hat
+	{
+		public HatNames name;
+		public Sprite hatSprite;
+	}
+
+	public Hat[] hats;
+
+	// Only one hatmanager at a time
+	private static HatManager _instance = null;
+
+	public static HatManager Instance 
+	{
+		get
+		{
+			//If _instance hasn't been set yet, we grab it from the scene!
+			//This will only happen the first time this reference is used.
+			// It will fail if there isn't a hatmanager prefab in the scene.
+			// TODO make it add a hatmanager itself perhaps? 
+			if(_instance == null) {
+				_instance = GameObject.FindObjectOfType<HatManager>();
+				if (!_instance) {
+					Debug.LogError("No Hat Manager Prefab in scene");
+				}
+			}
+			return _instance;
+		}
+	}
+
+
+	/// <summary>
+	/// Gets the hat sprite associated with the assigned enum.
+	/// </summary>
+	/// <returns>The hat sprite.</returns>
+	/// <param name="hatName">The HatName Enum.</param>
+	public Sprite GetHatSprite(HatNames hatName) {
+		// Seleects one (and ONLY ONE) of the sprites to return,
+		// Will fail if more sprites have same enum assigned
+		var sprite = hats.Single(h => h.name == hatName).hatSprite;
+		return sprite;
+	}
+}
