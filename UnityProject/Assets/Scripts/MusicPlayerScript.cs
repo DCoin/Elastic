8097efb8295cb9758df5aeb89c610e0cd398f3a2
aholdt <alexanderholdt@gmail.com>
2014-12-01 10:53:28 +0100

    MusicPlayer Next track = F9 -  Prev track = F8

---
+++ UnityProject/Assets/Scripts/MusicPlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayerScript : MonoBehaviour {
+
+
+	public AudioClip[] BackgroundMusicClips;
+
+	private int musicindex = 0;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKeyUp(KeyCode.F8)){
+			musicindex++;
+			if (musicindex > BackgroundMusicClips.Length-1)
+				musicindex = 0;
+			audio.clip = BackgroundMusicClips[musicindex];
+			audio.Play();
+			}
+		else if(Input.GetKeyUp(KeyCode.F9)){
+			musicindex--;
+			if (musicindex < 0)
+				musicindex = BackgroundMusicClips.Length-1;
+			audio.clip = BackgroundMusicClips[musicindex];
+			audio.Play();
+		}
+	}
+}
