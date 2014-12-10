42f4e13ace51f9e621c55ac81f75140d7d1b8edf
DCoin <pokedcoin@gmail.com>
2014-12-10 16:19:08 +0100

    Added lethal platform script

---
+++ UnityProject/Assets/Scripts/LethalPlatform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LethalPlatform : MonoBehaviour {
+	public float respawnTime = 7;
+
+	private DuelMode duelMode;
+
+	void Start() {
+		duelMode = GameObject.FindObjectOfType(typeof(DuelMode)) as DuelMode;
+	}
+
+	void OnCollisionStay2D (Collision2D col) {
+		var squad = col.collider.GetComponentInParent<Squad> ();
+		if (squad != null) squad.Kill(duelMode != null ? duelMode.respawnTime : respawnTime);
+	}
+}
