a73549698e64983b43855d134be102ffbd93019e
DCoin <pokedcoin@gmail.com>
2014-12-11 17:19:31 +0100

    Simplified the levelDivider + Prefab
    Added a prefab for the level divider and simplified it

--- UnityProject/Assets/Scripts/LevelDivider.cs
+++ UnityProject/Assets/Scripts/LevelDivider.cs
@@ -3,20 +3,11 @@ using System.Collections;
 
 public class LevelDivider : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	void OnCollisionEnter2D(Collision2D col) {
 		// New rope
 		var squad = col.gameObject.GetComponentInParent<Squad> ();
-		if (squad != null && squad.pickup != null) {
+		var pickup = col.gameObject.GetComponentInParent<PickupScript> ();
+		if (squad != null && squad.pickup != null || pickup != null && !pickup.pickupAble) {
 			transform.collider2D.isTrigger = true;
 		}
 		// Old rope
@@ -27,23 +18,10 @@ public class LevelDivider : MonoBehaviour {
 
 	void OnTriggerExit2D(Collider2D col) 
 	{
-		StartCoroutine (WaitAndSetTrigger(col.transform.root));
-	}
-
-	IEnumerator WaitAndSetTrigger(Transform squad)
-	{  
-		yield return new WaitForSeconds(3);
-		Mesh mesh = GetComponent<MeshFilter> ().mesh;
-		float width = mesh.bounds.size.x;
-		bool removetrigger = true;
-		foreach (Collider2D col in squad.GetComponentsInChildren<Collider2D>()) {
-			if (col.transform.position.x > transform.position.x-width/2 && col.transform.position.x < transform.position.x+width/2) {
-				removetrigger = false;
-				break;
-			}
+		if (collider2D.isTrigger == false) return;
+		var p = Physics2D.OverlapArea (collider2D.bounds.min, collider2D.bounds.max, (1<<8) | (1<<12) | (1<<13));
+		if (p == null) {
+			transform.collider2D.isTrigger = false;
 		}
-		if (removetrigger) {
-						transform.collider2D.isTrigger = false;
-		} else {  WaitAndSetTrigger(squad); }
 	}
 }
