a8c4a9bd5471abf58d0348502f0b3a3c3fafc9f2
kasraman1000 <minutegreywing@gmail.com>
2014-12-12 21:58:19 +0100

    Added respawn points to tutorial
    :stuck_out_tongue_winking_eye:

---
+++ UnityProject/Assets/Scripts/Tutorial/RespawnPoint.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RespawnPoint : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D col) {
+		// If we collide with a player, set its respawnpoint to this
+		if (col.gameObject.GetComponent<PlayerController>()) {
+			var squad = col.gameObject.GetComponentInParent<Squad>();
+			if (squad == null) {
+				Debug.LogError("This player is not part of a squad!");
+				gameObject.SetActive(false);
+				return;
+			}
+
+			squad.RespawnPoint = gameObject.transform.position;
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Tutorial/TutorialMode.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialMode : MonoBehaviour {
+	
+	public GameObject squad1;
+	public GameObject squad2;
+
+	public float killRespawnTime = 2.0F;
+
+	private Squad sq1;
+	private Squad sq2;
+
+	// Use this for initialization
+	void Start () {
+		// Get refs to squad scripts
+		sq1 = squad1.GetComponent<Squad>() as Squad;
+		sq2 = squad2.GetComponent<Squad>() as Squad;
+		
+		// Set up kill on squads and get the rope colors
+		// We assume Squads only have one rope
+		var rope = squad1.GetComponentInChildren<RopeCasting> ();
+		if (rope != null) {
+			rope.killActions += () => sq1.Kill(killRespawnTime);
+			//sq1color = rope.ropeMaterial.color;
+		}
+		rope = squad2.GetComponentInChildren<RopeCasting> ();
+		if (rope != null) {
+			rope.killActions += () => sq2.Kill(killRespawnTime);
+			//sq2color = rope.ropeMaterial.color;
+		}
+	}
+}
